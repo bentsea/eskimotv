586c30e217f64dd15ad3dd5863ec3f7fee54dd4b
VerbGardner <brokenmind@gmail.com>
2021-09-17 21:09:11 -0500

    Minor updates and html fixes.

--- app/admin/files.py
+++ app/admin/files.py
@@ -15,6 +15,7 @@ def save_cover_image(img,article_slug):
     new_height = int(img.height * (max_width / img.width))
     img = img.resize((max_width,new_height))
     img = resizeimage.resize_crop(img,[1920,900])
+    img = img.convert("RGB")
     img.save(f"{static_file_path}{cover_image_path}{img_name}",optimize=True,quality=65)
     return f"{cover_image_path}{img_name}"
 
